fbcef7f639d0cd84bd48440325e2ae436ff5293a
Nourajazbeh <dr.noorj19@gmail.com>
2024-05-15 15:34:42 +0200

    added tailwindcss

---
+++ src/Page/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+function About() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">About Page</h1>
+        <p>This is the about page.</p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
---
+++ src/Page/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Contact() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Contact Page</h1>
+        <p>This is the contact page.</p>
+      </div>
+    );
+  };
+  
+  export default Contact;
